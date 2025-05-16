147ebdb6af6768a6e1041d4c605e3ea95c5002f7
Evans Elabo <evans.elabo@amalitech.com>
2025-05-16 01:31:39 +0000

    feat(cards-data): add initial cards data for event categorization

---
+++ src/lib/data/cards-data.ts
@@ -0,0 +1,17 @@
+export const CARDS_DATA = [
+  {
+    title: "All events",
+    value: "4",
+    description: "All events created",
+  },
+  {
+    title: "Ongoing events",
+    value: "3",
+    description: "Published events still ongoing",
+  },
+  {
+    title: "Completed events",
+    value: "1",
+    description: "All past events",
+  },
+];
