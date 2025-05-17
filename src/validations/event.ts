556d67ef7e504fe7864afbcee3032fecfdaa3046
Evans Elabo <evans.elabo@amalitech.com>
2025-05-17 19:23:17 +0000

    fix(event-validation): make voting and nomination period fields optional

--- src/validations/event.ts
+++ src/validations/event.ts
@@ -3,10 +3,10 @@ import * as z from "zod";
 export const eventFormSchema = z.object({
   name: z.string().min(1, "Name is required"),
   description: z.string().min(1, "Description is required"),
-  voting_start_period: z.date(),
-  voting_end_period: z.date(),
-  nomination_start_period: z.date(),
-  nomination_end_period: z.date(),
+  voting_start_period: z.date().optional(),
+  voting_end_period: z.date().optional(),
+  nomination_start_period: z.date().optional(),
+  nomination_end_period: z.date().optional(),
 });
 
 export type EventFormValues = z.infer<typeof eventFormSchema>;
