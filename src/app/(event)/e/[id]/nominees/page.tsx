07a813f3993fd55098ce067ff3791757a183417c
Evans Elabo <evans.elabo@amalitech.com>
2025-05-17 02:21:23 +0000

    refactor(nominees): remove dummy data from NomineesPage component

--- src/app/(event)/e/[id]/nominees/page.tsx
+++ src/app/(event)/e/[id]/nominees/page.tsx
@@ -1,107 +1,6 @@
 import { Spinner } from "@/components/spinner";
 import { Suspense } from "react";
 import { NomineesDataTable } from "./_components/data-table";
-import { Nominee } from "./_components/columns";
-
-// Dummy data for nominees
-const dummyNominees: Nominee[] = [
-  {
-    id: "1",
-    fullName: "John Smith",
-    category: "Best Actor",
-    photo: "https://i.pravatar.cc/150?img=1",
-    code: "AC01",
-    totalVotes: 125,
-  },
-  {
-    id: "2",
-    fullName: "Emma Johnson",
-    category: "Best Actress",
-    photo: "https://i.pravatar.cc/150?img=5",
-    code: "AC02",
-    totalVotes: 142,
-  },
-  {
-    id: "3",
-    fullName: "Michael Williams",
-    category: "Best Actor",
-    photo: "https://i.pravatar.cc/150?img=3",
-    code: "AC03",
-    totalVotes: 98,
-  },
-  {
-    id: "4",
-    fullName: "Sophia Brown",
-    category: "Best Actress",
-    photo: "https://i.pravatar.cc/150?img=9",
-    code: "AC04",
-    totalVotes: 113,
-  },
-  {
-    id: "5",
-    fullName: "Daniel Jones",
-    category: "Best Supporting Actor",
-    photo: "https://i.pravatar.cc/150?img=4",
-    code: "SU05",
-    totalVotes: 87,
-  },
-  {
-    id: "6",
-    fullName: "Olivia Davis",
-    category: "Best Supporting Actress",
-    photo: "https://i.pravatar.cc/150?img=8",
-    code: "SU06",
-    totalVotes: 92,
-  },
-  {
-    id: "7",
-    fullName: "Robert Miller",
-    category: "Best Supporting Actor",
-    photo: "https://i.pravatar.cc/150?img=7",
-    code: "SU07",
-    totalVotes: 76,
-  },
-  {
-    id: "8",
-    fullName: "Ava Wilson",
-    category: "Best Supporting Actress",
-    photo: "https://i.pravatar.cc/150?img=2",
-    code: "SU08",
-    totalVotes: 81,
-  },
-  {
-    id: "9",
-    fullName: "James Taylor",
-    category: "Best Director",
-    photo: "https://i.pravatar.cc/150?img=11",
-    code: "DI01",
-    totalVotes: 156,
-  },
-  {
-    id: "10",
-    fullName: "Charlotte Anderson",
-    category: "Best Director",
-    photo: "https://i.pravatar.cc/150?img=10",
-    code: "DI02",
-    totalVotes: 143,
-  },
-  {
-    id: "11",
-    fullName: "Thomas White",
-    category: "Best Screenplay",
-    photo: "https://i.pravatar.cc/150?img=12",
-    code: "SC01",
-    totalVotes: 112,
-  },
-  {
-    id: "12",
-    fullName: "Isabella Clark",
-    category: "Best Screenplay",
-    photo: "https://i.pravatar.cc/150?img=19",
-    code: "SC02",
-    totalVotes: 103,
-  },
-];
 
 export default function NomineesPage() {
   return (
@@ -126,7 +25,7 @@ export default function NomineesPage() {
             </div>
           }
         >
-          <NomineesDataTable data={dummyNominees} />
+          <NomineesDataTable />
         </Suspense>
       </div>
     </section>
