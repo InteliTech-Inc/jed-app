669cffa3015c67cfd6ba8e49d95ccd2e46d60b40
Evans Elabo <evans.elabo@amalitech.com>
2025-05-17 02:21:57 +0000

    refactor(nominees): streamline EditNomineeForm by removing unused props and integrating category fetching

--- src/app/(event)/e/[id]/nominees/_components/edit-nominee-form.tsx
+++ src/app/(event)/e/[id]/nominees/_components/edit-nominee-form.tsx
@@ -13,24 +13,30 @@ import { Nominee } from "./columns";
 import { useState, useRef, useEffect } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Label } from "@/components/ui/label";
-import { generateCode } from "@/lib/utils";
-import { IconRefresh, IconCamera } from "@tabler/icons-react";
+import { IconCamera } from "@tabler/icons-react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/constants/query-keys";
+import QUERY_FUNCTIONS from "@/lib/functions/client";
+import { CategoryResponse } from "./create-nominee-modal";
+import { toast } from "sonner";
+import { AxiosError } from "axios";
+import { formatJedError } from "@/lib/utils";
+import { DrawerClose, DrawerFooter } from "@/components/ui/drawer";
+import { Spinner } from "@/components/spinner";
 
 interface EditNomineeFormProps {
   nominee: Nominee;
-  categories?: string[]; // maybe the categories will be fetched from the parent component
-  onFormChange?: (updatedNominee: Nominee) => void;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export function EditNomineeForm({
   nominee,
-  categories = [],
-  onFormChange,
-}: EditNomineeFormProps) {
+  setOpen,
+}: Readonly<EditNomineeFormProps>) {
   const [photoPreview, setPhotoPreview] = useState(nominee.photo);
   const [formData, setFormData] = useState({
     id: nominee.id,
-    fullName: nominee.fullName,
+    full_name: nominee.full_name,
     category: nominee.category,
     code: nominee.code,
     photo: nominee.photo,
@@ -38,135 +44,148 @@ export function EditNomineeForm({
   const [file, setFile] = useState<File | null>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const [availableCategories, setAvailableCategories] = useState<string[]>([
-    "Best Actor",
-    "Best Actress",
-    "Best Supporting Actor",
-    "Best Supporting Actress",
-    "Best Director",
-    "Best Screenplay",
-  ]);
+  const { fetchCategories, updateNominee } = QUERY_FUNCTIONS;
+  const queryClient = useQueryClient();
+  const { data: categories } = useQuery({
+    queryKey: [QUERY_KEYS.CATEGORIES],
+    queryFn: fetchCategories,
+  });
 
   useEffect(() => {
-    if (onFormChange) {
-      onFormChange(formData);
-    }
-  }, [formData, onFormChange]);
+    setFormData({
+      id: nominee.id,
+      full_name: nominee.full_name,
+      category: nominee.category,
+      code: nominee.code,
+      photo: nominee.photo,
+    });
+    setPhotoPreview(nominee.photo);
+  }, []);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files[0]) {
-      const selectedFile = e.target.files[0];
+    const selectedFile = e.target.files?.[0];
+    if (selectedFile) {
       setFile(selectedFile);
       const fileUrl = URL.createObjectURL(selectedFile);
       setPhotoPreview(fileUrl);
-      setFormData({ ...formData, photo: fileUrl });
+      setFormData((prev) => ({ ...prev, photo: fileUrl }));
     }
   };
 
   const handleCategoryChange = (value: string) => {
-    setFormData({ ...formData, category: value });
-  };
-
-  const handleGenerateCode = () => {
-    const newCode = generateCode();
-    setFormData({ ...formData, code: newCode });
+    setFormData((prev) => ({ ...prev, category: value }));
   };
 
   const triggerFileInput = () => {
     fileInputRef.current?.click();
   };
 
+  const { mutate: updateExistingNominee, isPending } = useMutation({
+    mutationKey: [QUERY_KEYS.NOMINEES],
+    mutationFn: async (payload: { data: any; id: string }) => {
+      return await updateNominee(payload.data, payload.id);
+    },
+    onSuccess: () => {
+      toast.success("Nominee updated successfully");
+      setOpen(false);
+    },
+    onError: (error: AxiosError) => {
+      if (error instanceof Error) {
+        toast.error(formatJedError(error));
+      } else {
+        toast.error("An error occurred while creating the nominee.");
+      }
+    },
+  });
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    updateExistingNominee({ data: formData, id: nominee.id });
+    queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.NOMINEES] });
   };
 
   return (
-    <form id="edit-nominee-form" onSubmit={handleSubmit} className="space-y-6">
-      <div className="flex justify-center">
-        <div className="relative">
-          <Avatar className="size-24">
-            <AvatarImage src={photoPreview} alt={formData.fullName} />
-            <AvatarFallback>{formData.fullName.charAt(0)}</AvatarFallback>
-          </Avatar>
-          <Button
-            type="button"
-            size="icon"
-            className="bg-accent text-accent-foreground hover:bg-accent/90 absolute -bottom-2 -left-2 size-8 rounded-full shadow-md"
-            onClick={triggerFileInput}
-          >
-            <IconCamera className="size-4" />
-            <span className="sr-only">Upload photo</span>
-          </Button>
-          <Input
-            ref={fileInputRef}
-            id="photo"
-            name="photo"
-            type="file"
-            accept="image/*"
-            onChange={handleFileChange}
-            className="hidden"
-          />
+    <form
+      id="edit-nominee-form"
+      onSubmit={handleSubmit}
+      className="flex h-full flex-col justify-end"
+    >
+      <div className="space-y-6">
+        <div className="flex justify-center">
+          <div className="relative">
+            <Avatar className="size-24">
+              <AvatarImage src={photoPreview} alt={formData.full_name} />
+              <AvatarFallback>{formData.full_name.charAt(0)}</AvatarFallback>
+            </Avatar>
+            <Button
+              type="button"
+              size="icon"
+              className="bg-accent text-accent-foreground hover:bg-accent/90 absolute -bottom-2 -left-2 size-8 rounded-full shadow-md"
+              onClick={triggerFileInput}
+            >
+              <IconCamera className="size-4" />
+              <span className="sr-only">Upload photo</span>
+            </Button>
+            <Input
+              ref={fileInputRef}
+              id="photo"
+              name="photo"
+              type="file"
+              accept="image/*"
+              onChange={handleFileChange}
+              className="hidden"
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="space-y-2">
-        <Label htmlFor="fullName">Full Name</Label>
-        <Input
-          id="fullName"
-          name="fullName"
-          placeholder="John Doe"
-          value={formData.fullName}
-          onChange={handleInputChange}
-        />
-      </div>
-
-      <div className="space-y-2">
-        <Label htmlFor="category">Category</Label>
-        <Select value={formData.category} onValueChange={handleCategoryChange}>
-          <SelectTrigger id="category" className="w-full">
-            <SelectValue placeholder="Select a category" />
-          </SelectTrigger>
-          <SelectContent>
-            {availableCategories.map((category) => (
-              <SelectItem key={category} value={category}>
-                {category}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      </div>
-
-      <div className="space-y-2">
-        <Label htmlFor="code">Code</Label>
-        <div className="flex gap-2">
+        <div className="space-y-2">
+          <Label htmlFor="full_name">Full Name</Label>
           <Input
-            id="code"
-            name="code"
-            placeholder="Click to generate"
-            value={formData.code}
+            id="full_name"
+            name="full_name"
+            placeholder="John Doe"
+            value={formData.full_name}
             onChange={handleInputChange}
-            readOnly
           />
-          <Button
-            type="button"
-            variant="secondary"
-            onClick={handleGenerateCode}
-            size="sm"
-            className="h-9 flex-shrink-0 rounded-full text-sm"
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="category">Category</Label>
+          <Select
+            value={formData?.category}
+            onValueChange={handleCategoryChange}
           >
-            Generate
-            <IconRefresh className="ml-1 h-2 w-2" />
-          </Button>
+            <SelectTrigger id="category" className="w-full">
+              {formData?.category || "Select a category"}
+            </SelectTrigger>
+            <SelectContent>
+              {categories?.data.categories?.map(
+                (category: CategoryResponse) => (
+                  <SelectItem key={category.id} value={category.id}>
+                    {category.name}
+                  </SelectItem>
+                ),
+              )}
+            </SelectContent>
+          </Select>
         </div>
       </div>
+      <div className="h-10" />
 
-      {/* Add some bottom spacing */}
-      <div className="h-10"></div>
+      <DrawerFooter className="mt-auto ml-auto flex flex-row gap-2">
+        <Button type="submit" className="h-10" disabled={isPending}>
+          {isPending ? <Spinner /> : "Save changes"}
+        </Button>
+        <DrawerClose asChild>
+          <Button variant="outline" className="h-10">
+            Cancel
+          </Button>
+        </DrawerClose>
+      </DrawerFooter>
     </form>
   );
 }
