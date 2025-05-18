c8d4264d920067e33f43a08069352603c43411e8
Evans Elabo <evans.elabo@amalitech.com>
2025-05-18 14:19:12 +0000

    feat(event-page): add metadata generation for event details including Open Graph and Twitter cards

--- src/app/(event)/e/[id]/page.tsx
+++ src/app/(event)/e/[id]/page.tsx
@@ -1,8 +1,44 @@
+import { authAxios } from "@/providers/api-client";
+
+export async function generateMetadata({
+  params,
+}: Readonly<{
+  params: Promise<{ id: string }>;
+}>) {
+  const { id } = await params;
+  const response = await authAxios.get(`/events/${id}`);
+  const event = response.data.data;
+  return {
+    title: `${event.name} | Event Management`,
+    description: event.description,
+    openGraph: {
+      title: event.name,
+      description: event.description,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/e/${event.id}`,
+      siteName: "JED",
+      images: [
+        {
+          url: event.image,
+          width: 1200,
+          height: 630,
+          alt: event.name,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: event.name,
+      description: event.description,
+      images: [event.image],
+    },
+  };
+}
+
 export default async function SingleEventPage({
   params,
-}: {
+}: Readonly<{
   params: Promise<{ id: string }>;
-}) {
+}>) {
   const { id } = await params;
   return (
     <div>
