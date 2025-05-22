c4901dbce914aca754e44078120a6ac42b8fc748
Evans Elabo <evans.elabo@amalitech.com>
2025-05-22 21:47:04 +0000

    refactor(no-nominations): simplify NoNominations component by removing eventName prop and adjusting layout

--- src/app/(event)/e/[id]/nominations/_components/no-nominations.tsx
+++ src/app/(event)/e/[id]/nominations/_components/no-nominations.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import EmptyStateImage from "@/assets/empty-state-illustration.png";
 
-export function NoNominations({ eventName }: Readonly<{ eventName: string }>) {
+export function NoNominations() {
   return (
     <div className="my-auto grid h-fit w-full place-items-center">
-      <section className="h-48 w-full">
+      <section className="h-36 w-full">
         <Image
           src={EmptyStateImage}
           alt="No nominations found"
@@ -16,8 +16,8 @@ export function NoNominations({ eventName }: Readonly<{ eventName: string }>) {
       <p className="text-2xl font-semibold text-gray-800">
         No nominations found
       </p>
-      <p className="mt-4 max-w-md text-center text-neutral-500">
-        The event {eventName} does not have any nominations from the public yet.
+      <p className="mt-1 mb-4 max-w-md text-center text-neutral-500">
+        This event does not have any nominations from the public yet.
       </p>
     </div>
   );
